4fc610c1acc8a1f6a1db69260f6edb755075286f
Andrea Cavalli <andrea@cavallium.it>
2021-01-27 03:04:20 +0100

    Update AsyncTdEasy.java

--- src/main/java/it/tdlight/tdlibsession/td/easy/AsyncTdEasy.java
+++ src/main/java/it/tdlight/tdlibsession/td/easy/AsyncTdEasy.java
@@ -58,7 +58,7 @@ public class AsyncTdEasy {
 
 	private final Logger logger;
 
-	private static final Scheduler scheduler = Schedulers.newSingle("AsyncTdEasy", true);
+	private static final Scheduler scheduler = Schedulers.parallel();
 	private final Many<AuthorizationState> authState = Sinks.many().replay().latest();
 	private final Many<Boolean> requestedDefinitiveExit = Sinks.many().replay().latestOrDefault(false);
 	private final Many<TdEasySettings> settings = Sinks.many().replay().latest();
