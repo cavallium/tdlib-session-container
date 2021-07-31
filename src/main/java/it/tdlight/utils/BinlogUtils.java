21097c51358c3e62715b1d05a1eee362e7ee5393
Andrea Cavalli <andrea@cavallium.it>
2021-07-31 18:01:20 +0200

    Use flatMapIterable when possible

--- src/main/java/it/tdlight/utils/BinlogUtils.java
+++ src/main/java/it/tdlight/utils/BinlogUtils.java
@@ -78,11 +78,11 @@ public class BinlogUtils {
 				.rxReadFile(binlogPath.toString()).as(MonoUtils::toMono)
 				.flatMap(buffer -> vertxFilesystem
 						.rxReadDir(sessionPath.toString(), "^(?!td.binlog$).*").as(MonoUtils::toMono)
-						.flatMapMany(Flux::fromIterable)
+						.flatMapIterable(list -> list)
 						.doOnNext(file -> logger.debug("Deleting session file {}", file))
 						.flatMap(file -> vertxFilesystem.rxDeleteRecursive(file, true).as(MonoUtils::toMono))
 						.then(vertxFilesystem.rxReadDir(mediaPath.toString(), "^(?!td.binlog$).*").as(MonoUtils::toMono))
-						.flatMapMany(Flux::fromIterable)
+						.flatMapIterable(list -> list)
 						.doOnNext(file -> logger.debug("Deleting media file {}", file))
 						.flatMap(file -> vertxFilesystem.rxDeleteRecursive(file, true).as(MonoUtils::toMono))
 						.onErrorResume(ex -> Mono.empty())
