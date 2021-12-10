8b0220ccfc9d8da1499c1ee4276547802c748ede
Andrea Cavalli <andrea@cavallium.it>
2021-12-10 02:23:01 +0100

    Update dependencies and move libraryversion

--- src/main/java-templates/it/tdlight/reactiveapi/generated/LibraryVersion.java
+++ src/main/java-templates/it/tdlight/reactiveapi/generated/LibraryVersion.java
@@ -1,4 +1,4 @@
-package org.tdlibsessioncontainer.utils.generated;
+package it.tdlight.reactiveapi.generated;
 public final class LibraryVersion {
     public static final String VERSION = "${project.version}";
 }
